fdfbada262faa1fbaf52d501a7d7622be147af36
TellemBigAlsentya <alexhoffman26@gmail.com>
2022-12-06 13:27:16 -0800

    Added the function allowing filtering by Pokemon type

--- Main.js
+++ Main.js
@@ -1,7 +1,10 @@
 const PokeAPI = 'https://pokeapi.co/api/v2/pokemon/';
 const pokemonObjects = [];
 const searchInput = document.getElementById('pokemon-search');
+const typeFilter = document.querySelectorAll('li.filterType');
 searchInput.addEventListener('keyup', searchPokedex);
+typeFilter.forEach((filter) => filter.addEventListener('click', filterPokemon));
+
 // Use a for loop to append i to the PokeAPI URL to cycle through
 // vanilla pokemon (1-150) and return an array of objects representing
 // each pokemon and the key value pairs we need
@@ -19,7 +22,6 @@ async function getPokemon() {
     for(let i = 1; i <= 151; i++) {
         const pokeAPIResponse = await fetch(`${PokeAPI}${i}`);
         const json = await pokeAPIResponse.json();
-        console.log(json);
         //Use object destructruting to access nested type characteristic within objects/arrays. These
         // types are then saved in an empty array then used later with the ToString method 
         const iveGotAType = []; 
@@ -35,7 +37,6 @@ async function getPokemon() {
             weight: `${json.weight}`
         };
         pokemonObjects.push(pokemon);
-        console.log(pokemon);
     }
 }
 // create a function that dynamically created pokedex cell DOM elements from each
@@ -44,6 +45,7 @@ function createDOMElements() {
     let pokedexContainer = document.createElement('div');
     pokedexContainer.classList.add('pokedex-container');
     document.body.appendChild(pokedexContainer);
+    // spacer is for making sure the pokedex container is spaced correctly in .png frame 
     let spacer = document.createElement('div',);
     spacer.classList.add('space');
     document.body.appendChild(spacer);
@@ -52,15 +54,14 @@ function createDOMElements() {
         let entrySprite = document.createElement('img');
         let entryText = document.createElement('div');
         entrySprite.classList.add('pokemon-image');
-        entry.classList.add('cell');
-        entry.addEventListener('click', highlightPokemon); 
+        entry.classList.add('cell'); 
 
         entrySprite.src = `${pokemonObjects[i].image}`;
         entryText.innerHTML = 
             `<ul>
                 <li>name: ${pokemonObjects[i].name.toUpperCase()}</li>
                 <li>number: ${pokemonObjects[i].number}</li>
-                <li>types: ${pokemonObjects[i].types}</li>
+                <li class="types">types: ${pokemonObjects[i].types}</li>
                 <li>weight: ${pokemonObjects[i].weight / 10} Kg </li>
             </ul>`;
         pokedexContainer.appendChild(entry);
@@ -75,9 +76,7 @@ pokedexSetup();
 // the name of each pokemon 
 
 function searchPokedex(event) {
-    console.log(event);
     let searchTerm = event.target.value.toUpperCase();
-    console.log(searchTerm);
     let pokemonNames = document.querySelectorAll('.cell');
     for(let i = 0; i < pokemonObjects.length; i++) {
         let pokemonCardName = pokemonNames[i].getElementsByTagName('li')[0].innerText.slice(6);
@@ -93,10 +92,19 @@ function searchPokedex(event) {
 
     }
 }
-// I want to let users of the pokedex  
 
-function highlightPokemon(event) {
-    let clickedCell = event.target; 
-
+function filterPokemon(event) {
+    let filterClick = event.target.innerHTML.toLowerCase();    
+    // I want to use the inner HTML of the targetted filter to iterate through the created DOM elements and only display
+    // pokemon with matching types. 
+    let pokemonTypes = document.querySelectorAll('.cell');
+    for(let i = 0; i < pokemonObjects.length; i++) {
+        let eachType = pokemonTypes[i].getElementsByTagName('li')[2].innerText.slice(7);
+        if(eachType.indexOf(filterClick) > -1) {
+            pokemonTypes[i].classList.remove('hidden');
+        } else {
+            pokemonTypes[i].classList.add('hidden');
+        }
+    }
 }
 
